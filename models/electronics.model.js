f412441b14b2675b79e1bf2eefba915d4cf6ed73
divyanshu40 <divyanshuprajapati40@gmail.com>
2025-05-04 03:06:52 +0530

    electronics.model.js file updated

--- models/electronics.model.js
+++ models/electronics.model.js
@@ -1216,5 +1216,207 @@ const mobileCablesSchema = new mongoose.Schema({
 });
 
 const mobileChargers = new mongoose.Schema({
-    generalFeatures: {}
+    generalFeatures: {
+        name: {
+            type: String
+        },
+        brand: {
+            type: String
+        },
+        modelName: {
+            type: String
+        },
+        modelNumber: {
+            type: String
+        },
+        outputInterface: {
+            type: String
+        },
+        ledIndicator: {
+            type: Boolean
+        },
+        display: {
+            type: Boolean
+        },
+        batteryType: {
+            type: String
+        },
+        connectorType: {
+            type: String
+        },
+        dimensions: {
+            width: {
+                type: Number
+            },
+            height: {
+                type: Number
+            },
+            depth: {
+                type: Number
+            }
+        },
+        weight: {
+            type: Number
+        },
+        otherGeneralFeatures: {
+            type: Map,
+            of: mongoose.Schema.Types.Mixed
+        }
+    },
+    powerFeatures: {
+        powerInput: {
+            type: Number
+        },
+        outputCurrent: {
+            type: Number
+        },
+        outputWattage: {
+            type: Number
+        },
+        otherFeatures: {
+            type: Map,
+            of: mongoose.Schema.Types.Mixed
+        }
+    },
+    numberOfPorts: {
+        type: Number
+    },
+    ratings: {
+        Number
+    },
+    averageRating: {
+        type: Number
+    },
+    reviews: {
+        type: Number
+    },
+    orignalPrice: {
+        type: Number
+    },
+    discount: {
+        type: Number
+    },
+    discountedPrice: {
+        type: Number
+    },
+    offers: {
+        bankOffers: [
+            {
+                bankName: {
+                    type: String
+                },
+                offerOnBank: {
+                    type: Number
+                },
+                summary: {
+                    type: String
+                },
+                termsAndConditions: {
+                    type: String
+                }
+            }
+        ],
+        specialPriceOffers: {
+            offerPrice: {
+                type: Number
+            },
+            offerSummary: {
+                type: String
+            }
+        }
+    },
+    warranty: {
+        domesticWarranty: {
+            type: String
+        },
+        warrantySummary: {
+            type: String
+        },
+        warrantyServiceType: {
+            type: String
+        },
+        warrantyCoverage: {
+            type: String
+        },
+        notCoveredInWarranty: {
+            type: String
+        }
+    },
+    highlights: {
+        type: [String]
+    },
+    productImages: {
+        type: [String]
+    },
+    thumbnailImage: {
+        type: String
+    }
+}, {
+    timestamps: true
+});
+
+
+const smartWatches = new mongoose.Schema({
+    generalFeatures: {
+        name: {
+            type: String
+        },
+        brand: {
+            type: String
+        },
+        dialShape: {
+            type: String,
+            enum: ["Contemporary", "Curved", "Oval", "Rectangle", "Round", "Square", "Tonneau"]
+        },
+        displaySize: {
+            type: Number
+        },
+        strapColor: {
+            type: [String]
+        },
+        displayType: {
+            type: String,
+            enum: [
+                "AMOLED",
+                "Dot Matrix",
+                "HD",
+                "IPS HD",
+                "IPS LCD",
+                "IPS TFT",
+                "LCD",
+                "LED",
+                "Mineral",
+                "OLED",
+                "OLED Retina Display",
+                "Retina Display",
+                "Super AMOLED",
+                "TFT",
+                "TFT LED",
+                "Ultra HD"
+              ],
+        },
+        idealFor: {
+            type: String,
+            enum: ["Kids", "Women", "Men", "Unisex"]
+        },
+        os: {
+            type: String,
+            enum: ["Android", "Android & iOS", "iOS", "Windows", "Propretiary OS"]
+        },
+        features: {
+            types: [String],
+        },
+        strapMaterial: {
+            type: String
+        },
+        usage: {
+            type: String
+        },
+        modelName: {
+            type: String
+        },
+        modelNumber: {
+            type: String
+        }
+    }
 })
