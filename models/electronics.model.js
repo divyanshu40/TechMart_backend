06a0a0f9308cd7c027dcd19bc04b68e9bf76ebe7
divyanshu40 <divyanshuprajapati40@gmail.com>
2025-05-04 20:01:22 +0530

    electronics.model.js updated

--- models/electronics.model.js
+++ models/electronics.model.js
@@ -1417,6 +1417,143 @@ const smartWatches = new mongoose.Schema({
         },
         modelNumber: {
             type: String
+        },
+        otherGeneralFeatures: {
+            type: Map,
+            of: mongoose.Schema.Types.Mixed
         }
+    },
+    productDetails: {
+        sensor: {
+            type: [String]
+        },
+        compatibleDevice: {
+            type: String
+        },
+        notificationType: {
+            type: String
+        },
+        notifications: {
+            type: [String]
+        },
+        batteryLife: {
+            type: String
+        }
+    },
+    connectivityFeatures: {
+        attributes: {
+            type: Map,
+            of: mongoose.Schema.Types.Mixed
+        }
+    },
+    cameraAndDisplayFeatures: {
+        attributes: {
+            type: Map,
+            of: mongoose.Schema.Types.Mixed
+        }
+    },
+    fitnessAndWatchFunctions: {
+        attributes: {
+            type: Map,
+            of: mongoose.Schema.Types.Mixed
+        }
+    },
+    audioAndVideoFeatures: {
+        attributes: {
+            type: Map,
+            of: mongoose.Schema.Types.Mixed
+        }
+    },
+    dimensions: {
+        width:{
+            type: Number
+        },
+        height: {
+            type: Number
+        },
+        thickness: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        }
+    },
+    platformAndStorageFeatures: {
+        attributes: {
+            type: Map,
+            of: mongoose.Schema.Types.Mixed
+        }
+    },
+    ratings: {
+        Number
+    },
+    averageRating: {
+        type: Number
+    },
+    reviews: {
+        type: Number
+    },
+    orignalPrice: {
+        type: Number
+    },
+    discount: {
+        type: Number
+    },
+    discountedPrice: {
+        type: Number
+    },
+    offers: {
+        bankOffers: [
+            {
+                bankName: {
+                    type: String
+                },
+                offerOnBank: {
+                    type: Number
+                },
+                summary: {
+                    type: String
+                },
+                termsAndConditions: {
+                    type: String
+                }
+            }
+        ],
+        specialPriceOffers: {
+            offerPrice: {
+                type: Number
+            },
+            offerSummary: {
+                type: String
+            }
+        }
+    },
+    warranty: {
+        domesticWarranty: {
+            type: String
+        },
+        warrantySummary: {
+            type: String
+        },
+        warrantyServiceType: {
+            type: String
+        },
+        warrantyCoverage: {
+            type: String
+        },
+        notCoveredInWarranty: {
+            type: String
+        }
+    },
+    highlights: {
+        type: [String]
+    },
+    productImages: {
+        type: [String]
+    },
+    thumbnailImage: {
+        type: String
     }
-})
+}, {
+    timestamps: true
+});
