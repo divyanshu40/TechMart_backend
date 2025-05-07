56f52ff55862ab407e2ac8ccaf368c47c1cc873b
divyanshu40 <divyanshuprajapati40@gmail.com>
2025-05-08 02:37:47 +0530

    index.js file updated and vercel.json file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { mobile } = require("./models/mobile.model");
+const { fetchedMobileData } = require("./models/fetchedMobileData.model");
 const { initializeDatabase } = require("./db/db.connect");
 const {
     casesAndCovers,
@@ -40,12 +41,48 @@ async function getAllMobiles() {
     return { mobiles: mobiles };
 }
 
-// GET route to get all mobiles from the database
+// function to get mobile details by id
+async function getMobileDetailsById(productId) {
+    let mobileDetails = await mobile.findById(productId);
+    if (! mobileDetails) {
+        return null;
+    }
+    return { mobile: mobileDetails };
+}
+
+
+// POST route to add mobile data
+app.post("/mobiles/new", async (req, res) => {
+    const mobileData = req.body
+    try {
+        const addedMobileData = await new mobile(mobileData).save();
+        return res.status(201).json({ newMobile: addedMobileData });
+    } catch(error) {
+        res.status(500).json({ error: error.message });
+    }
+    
+});
+
+// GET route to get all mobiles
 app.get("/mobiles", async (req, res) => {
     try {
-        const response = await getAllMobiles();
+        let response = await getAllMobiles();
         if (response.mobiles.length === 0) {
-            return res.status(404).json({ message: "Mobiles not found"});
+            return res.status(404).json({ message: "Mobiles not found" });
+        }
+        return res.status(200).json(response);
+    } catch(error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// GET route to get mobile by id
+app.get("/mobiles/details/:id", async (req, res) => {
+    let productId = req.params.id;
+    try {
+        let response = await getMobileDetailsById(productId);
+        if (response === null) {
+            return res.status(404).json({ message: "Mobile details not found" });
         }
         return res.status(200).json(response);
     } catch(error) {
